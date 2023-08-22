71e82f5e321d375d528651f9c355685f42c75069
YagoGu <yago.gutierrez15@gmail.com>
2023-08-22 15:53:20 +0200

    iteration 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,62 @@
 import "./App.css";
 import contacts from "./contacts.json";
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function App() {
-  const [count, setFamous] = useState(contacts.slice(0, 5));
+  const [famousArray, setFamous] = useState(contacts.slice(0, 5));
 
   function deleteFamous (famousId) {
-    const filteredFamous = count.filter((famous) => {
+    const filteredFamous = famousArray.filter((famous) => {
       console.log(famous.id, famousId)
       return famous.id !== famousId
     })
     setFamous(filteredFamous);
   }
 
+  // function addRandom () {
+  //   const moreFamous = famousArray
+  //   const newFamous = Math.floor(Math.random()*contacts.length)
+  //   const filteredFamous = moreFamous.filter((famous) => {
+  //     if(famous.id !== moreFamous[newFamous].id) {
+  //       moreFamous.push(moreFamous[newFamous])
+  //       console.log(moreFamous[newFamous].id)
+  //     }
+  //   })
+  //   setFamous(filteredFamous);
+  // }
+
+  function sortByName() {
+    const sortedNames = famousArray.sort( (a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    })
+
+    const newArr = [...sortedNames];
+
+    setFamous(newArr);
+  }
+
+  function sortByPopularity() {
+    
+    const sortedPopularities = famousArray.sort( (a, b) => 
+      b.popularity - a.popularity
+    )
+    
+    const newArr = [...sortedPopularities];
+
+    setFamous(newArr);
+  }
+
   return (
     <div className="App">
+      <button onClick={() => addRandom() }>Add Random Contact</button>
+      <button onClick={() => sortByPopularity() }>Sort by Popularity</button>
+      <button onClick={() => sortByName() }>Sort by Name</button>
       <table>
           <tr>
             <th>Image</th>
@@ -25,7 +67,7 @@ function App() {
             <th>Action</th>
           </tr>
             {
-              count.map((famous) => {
+              famousArray.map((famous) => {
                 return (
                 <tr>
                   <td><img width="30px" src={famous.pictureUrl} alt="Profile picture" /></td>
